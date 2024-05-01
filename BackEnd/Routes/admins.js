8cbeaf628a9c5c3876f16538ec39c3c9f3c60e1f
João Gabriel Ferreira Mendes <144286571+GabsFerr22@users.noreply.github.com>
2024-05-01 01:42:03 -0300

    Delete BackEnd directory

--- BackEnd/Routes/admins.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getUsers, postUsers } from "../controllers/server.js";
-
-const router = express.Router()
-
-router.get("/", getUsers)
-router.post("/", postUsers )
-
-export default router;
